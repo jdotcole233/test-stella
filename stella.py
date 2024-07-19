d557f6d02daa90dca7f2a103122467fb7371de81
Jay Cole <jdotcole233@gmail.com>
2024-07-19 10:27:28 +0000

    stella added

---
+++ stella.py
@@ -0,0 +1,15 @@
+firstNumber = 5
+secondNumber = 10
+extraNumber = 15
+
+thirdNumber = firstNumber + secondNumber + extraNumber
+
+listOfNumber = list(range(1, 1_000_001))
+
+sum = 0
+
+for number in listOfNumber:
+  sum = sum + number
+
+
+print(sum)
